610cb01e4d22de57ec8ff7887ee54faa9bf58fcf
Jakub Zimnoch <j.m.zimnoch@gmail.com>
2025-03-12 23:58:56 +0100

    add headers to next.config.ts to serve wasm from public folder on prod

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["three"],
+  async headers() {
+    return [
+      {
+        source: "/web-ifc/:path*",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/wasm",
+          },
+        ],
+      },
+    ];
+  },
 
   /* config options here */
 };
