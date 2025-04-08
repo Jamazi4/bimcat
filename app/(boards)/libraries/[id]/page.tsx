d2fa83becaffd1d6a007abdcba8027319201097c
Jakub Zimnoch <j.m.zimnoch@gmail.com>
2025-04-09 00:46:15 +0200

    remove more unused vars and imports

--- app/(boards)/libraries/[id]/page.tsx
+++ app/(boards)/libraries/[id]/page.tsx
@@ -13,13 +13,13 @@ import {
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const { libraryInfo, frontendComponents } = await fetchLibraryComponents(id);
-  if (!id) return <div>Library doesn't exist</div>;
+  if (!id) return <div>Library does not exist</div>;
 
   if (!frontendComponents) return <div>No components found.</div>;
 
   return (
     <main className="w-full px-4 justify-center mx-auto">
-      <BreadCrumbs name={libraryInfo.libraryName} id={id} />
+      <BreadCrumbs name={libraryInfo.libraryName} />
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold my-6">{libraryInfo.libraryName}</h1>
       </div>
@@ -29,7 +29,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 };
 export default page;
 
-const BreadCrumbs = ({ name, id }: { name: string; id: string }) => {
+const BreadCrumbs = ({ name }: { name: string }) => {
   return (
     <Breadcrumb className="border-accent">
       <BreadcrumbList>
