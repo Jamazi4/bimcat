f77ac250208a3dba93e239a203afeb27d6f526f3
Jakub Zimnoch <j.m.zimnoch@gmail.com>
2025-04-09 00:39:07 +0200

    Sonner styling

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,26 @@
 "use client";
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { useTheme } from "next-themes";
+import { ToasterProps } from "sonner";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <>
-      <Toaster closeButton />
+      <Toaster
+        closeButton
+        theme={resolvedTheme as ToasterProps["theme"]}
+        toastOptions={{
+          classNames: {
+            closeButton:
+              "!bg-background !text-destructive !border-muted-foreground",
+            toast:
+              "!bg-background !text-primary border-1 !border-muted-foreground",
+          },
+        }}
+      />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
