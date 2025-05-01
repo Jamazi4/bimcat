7883235e0ca392a644c37b1084b55066d2e01cda
Jakub Zimnoch <j.m.zimnoch@gmail.com>
2025-05-01 12:51:10 +0200

    refactor LibraryList to a separate file

--- components/componentList/AddComponentToLibraryButton.tsx
+++ components/componentList/AddComponentToLibraryButton.tsx
@@ -10,34 +10,19 @@ import {
   DialogDescription,
 } from "../ui/dialog";
 import { addComponentToLibraryAction } from "@/utils/actions/libraryActions";
-import {
-  BookUp,
-  Check,
-  ChevronsUpDown,
-  FolderDot,
-  LoaderCircle,
-} from "lucide-react";
+import { BookUp, LoaderCircle } from "lucide-react";
 import { Button } from "../ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "../ui/command";
-import { cn } from "@/lib/utils";
 import { SelectedRow } from "@/utils/types";
 import { toast } from "sonner";
 import NameList from "./NameList";
 import TooltipActionTriggerButton from "./TooltipActionTriggerButton";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/lib/store";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/lib/store";
 import { fetchUserLibraries } from "@/lib/features/user/userSlice";
 import WarningMessage from "../global/WarningMessage";
 import InfoMessage from "../global/InfoMessage";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import DialogLibraryList from "./DialogLibraryList";
 
 const AddComponentToLibraryButton = ({
   components,
@@ -101,7 +86,7 @@ const AddComponentToLibraryButton = ({
   });
 
   const handleClick = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.stopPropagation();
     setDialogOpen(false);
@@ -127,7 +112,7 @@ const AddComponentToLibraryButton = ({
         onSettled: () => {
           setPending(false);
         },
-      }
+      },
     );
   };
 
@@ -162,7 +147,7 @@ const AddComponentToLibraryButton = ({
               />
             </DialogDescription>
           </DialogHeader>
-          <LibraryList
+          <DialogLibraryList
             setHighlighConstructiveIds={setHighlighConstructiveIds}
             anyComponentPrivate={anyComponentPrivate}
             setValue={setLibraryId}
@@ -191,125 +176,3 @@ const AddComponentToLibraryButton = ({
 };
 
 export default AddComponentToLibraryButton;
-
-const LibraryList = ({
-  setValue,
-  value,
-  setDisplayAlert,
-  setDisplayInfo,
-  componentIds,
-  anyComponentPrivate,
-  setHighlighConstructiveIds,
-}: {
-  setValue: Dispatch<SetStateAction<string>>;
-  setDisplayAlert: Dispatch<SetStateAction<boolean>>;
-  setDisplayInfo: Dispatch<SetStateAction<boolean>>;
-  value: string;
-  componentIds: string[];
-  anyComponentPrivate: boolean;
-  setHighlighConstructiveIds: Dispatch<SetStateAction<string[]>>;
-}) => {
-  const [open, setOpen] = useState(false);
-
-  type libraryListPosition = {
-    value: string;
-    label: string;
-    isPublic: boolean;
-    componentIds: string[];
-  };
-
-  const userState = useSelector((state: RootState) => state.userSlice);
-
-  const librariesContaining = userState.libraries.map((library) => {
-    const componentIdsInside = library.components.map(
-      (component) => component.id
-    );
-    let found = false;
-    for (const id of componentIds) {
-      if (componentIdsInside.includes(id)) {
-        found = true;
-      }
-    }
-    if (found) return library.id;
-  });
-
-  const libraries: libraryListPosition[] = userState.libraries.map((lib) => {
-    return {
-      value: lib.id,
-      label: lib.name,
-      isPublic: lib.isPublic,
-      componentIds: lib.components.map((comp) => comp.id),
-    };
-  });
-
-  const onSelect = (currentValue: string) => {
-    const selectedLibrary = libraries.find(
-      (lib) => lib.label === currentValue
-    )!;
-
-    const selectedLibraryPublic = selectedLibrary.isPublic!;
-    const displayAlertFlag = selectedLibraryPublic && anyComponentPrivate;
-
-    const alreadyInside = librariesContaining.includes(selectedLibrary?.value);
-
-    if (alreadyInside) {
-      if (!selectedLibrary) return;
-      setHighlighConstructiveIds(selectedLibrary?.componentIds);
-    } else {
-      setHighlighConstructiveIds([]);
-    }
-
-    setDisplayInfo(alreadyInside);
-    setDisplayAlert(displayAlertFlag);
-    setValue(selectedLibrary ? selectedLibrary.value : "");
-    setOpen(false);
-  };
-
-  return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-between"
-        >
-          {value
-            ? libraries.find((library) => library.value === value)?.label
-            : "Select library..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-full p-0">
-        <Command>
-          <CommandInput placeholder="Search library..." />
-          <CommandList>
-            <CommandEmpty>No library found.</CommandEmpty>
-            <CommandGroup>
-              {libraries.map((library) => {
-                const highlight = librariesContaining.includes(library.value);
-                return (
-                  <CommandItem
-                    className="w-90"
-                    key={library.value}
-                    value={library.label}
-                    onSelect={(currentValue) => onSelect(currentValue)}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        value === library.value ? "opacity-100" : "opacity-0"
-                      )}
-                    />
-                    {library.label}
-                    {highlight && <FolderDot className="absolute right-2" />}
-                  </CommandItem>
-                );
-              })}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
-  );
-};
